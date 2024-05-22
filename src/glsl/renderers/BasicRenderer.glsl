d2731dff0607d37a4fd28a875c2092a2b5806d45
Matija Ojo <i-matija.ojo@devrev.ai>
2024-05-22 13:45:34 +0200

    initial MCM, TODO: proper texture handling

--- src/glsl/renderers/BasicRenderer.glsl
+++ src/glsl/renderers/BasicRenderer.glsl
@@ -138,15 +138,19 @@ void main() {
     }
 
     // position: [0, 512] -> [-1, 1] (clip space)
+    // TODO: use textureSize()
     vec2 position = vec2(currPos - ivec2(256)) / 256.0;
 
     vPosition = position; // Send position to the fragment shader, which will write it to the texture
 
     // We have to compute the position in which we will render the pixel (the same way as we will do in the render/vertex shader)
+    // TODO: Simplfy this using textureSize()
     float y = float(100 * int(float(gl_VertexID) / 100.0)) / 100000.0;
-    float x = float(gl_VertexID% 100) / 100.0;
+    float x = float(gl_VertexID % 100) / 100.0;
     vec2 pos = vec2(x, y) * 2.0 - 1.0;
     gl_Position = vec4(pos, 0, 1);
+
+    // TODO: Use texture (64 x 1), use gl_VertexID
 }
 
 // #part /glsl/shaders/renderers/BASIC/integrate/fragment
@@ -154,15 +158,161 @@ void main() {
 #version 300 es
 precision mediump float;
 precision mediump sampler2D;
+precision mediump sampler3D;
+
+#define EPS 1e-5
+
+// #link /glsl/mixins/Photon
+@Photon
+// #link /glsl/mixins/intersectCube
+@intersectCube
+
+@constants
+@random/hash/pcg
+@random/hash/squashlinear
+@random/distribution/uniformdivision
+@random/distribution/square
+@random/distribution/disk
+@random/distribution/sphere
+@random/distribution/exponential
+
+@unprojectRand
+
+uniform sampler2D uPosition;
+uniform sampler2D uDirection;
+uniform sampler2D uTransmittance;
+uniform sampler2D uRadiance;
+
+uniform sampler3D uVolume;
+uniform sampler2D uTransferFunction;
+uniform sampler2D uEnvironment;
+
+uniform mat4 uMvpInverseMatrix;
+uniform vec2 uInverseResolution;
+uniform float uRandSeed;
+uniform float uBlur;
+
+uniform float uExtinction;
+uniform float uAnisotropy;
+uniform uint uMaxBounces;
+uniform uint uSteps;
 
 in vec2 vPosition;
 
-layout (location = 0) out vec4 oColor;
-layout (location = 1) out vec2 oPosition;
+layout (location = 0) out vec4 oPosition;
+layout (location = 1) out vec4 oDirection;
+layout (location = 2) out vec4 oTransmittance;
+layout (location = 3) out vec4 oRadiance;
+layout (location = 4) out vec2 oPositionRay;
+
+
+void resetPhoton(inout uint state, inout Photon photon) {
+    vec3 from, to;
+    unprojectRand(state, vPosition, uMvpInverseMatrix, uInverseResolution, uBlur, from, to);
+    photon.direction = normalize(to - from);
+    photon.bounces = 0u;
+    vec2 tbounds = max(intersectCube(from, photon.direction), 0.0);
+    photon.position = from + tbounds.x * photon.direction;
+    photon.transmittance = vec3(1);
+}
+
+vec4 sampleEnvironmentMap(vec3 d) {
+    vec2 texCoord = vec2(atan(d.x, -d.z), asin(-d.y) * 2.0) * INVPI * 0.5 + 0.5;
+    return texture(uEnvironment, texCoord);
+}
+
+vec4 sampleVolumeColor(vec3 position) {
+    vec2 volumeSample = texture(uVolume, position).rg;
+    vec4 transferSample = texture(uTransferFunction, volumeSample);
+    return transferSample;
+}
+
+float sampleHenyeyGreensteinAngleCosine(inout uint state, float g) {
+    float g2 = g * g;
+    float c = (1.0 - g2) / (1.0 - g + 2.0 * g * random_uniform(state));
+    return (1.0 + g2 - c * c) / (2.0 * g);
+}
+
+vec3 sampleHenyeyGreenstein(inout uint state, float g, vec3 direction) {
+    // generate random direction and adjust it so that the angle is HG-sampled
+    vec3 u = random_sphere(state);
+    if (abs(g) < EPS) {
+        return u;
+    }
+    float hgcos = sampleHenyeyGreensteinAngleCosine(state, g);
+    vec3 circle = normalize(u - dot(u, direction) * direction);
+    return sqrt(1.0 - hgcos * hgcos) * circle + hgcos * direction;
+}
+
+float max3(vec3 v) {
+    return max(max(v.x, v.y), v.z);
+}
+
+float mean3(vec3 v) {
+    return dot(v, vec3(1.0 / 3.0));
+}
+
 
 void main() {
-    oColor = vec4(1.0); // TODO: Compute actual color (MCM)
-    oPosition = vPosition;
+    Photon photon;
+    vec2 mappedPosition = vPosition * 0.5 + 0.5;
+    vec2 coord = gl_FragCoord.xy / 512.0;
+    photon.position = texture(uPosition, coord).xyz;
+    vec4 directionAndBounces = texture(uDirection, coord);
+    photon.direction = directionAndBounces.xyz;
+    photon.bounces = uint(directionAndBounces.w + 0.5);
+    photon.transmittance = texture(uTransmittance, coord).rgb;
+    vec4 radianceAndSamples = texture(uRadiance, coord);
+    photon.radiance = radianceAndSamples.rgb;
+    photon.samples = uint(radianceAndSamples.w + 0.5);
+
+    uint state = hash(uvec3(floatBitsToUint(mappedPosition.x), floatBitsToUint(mappedPosition.y), floatBitsToUint(uRandSeed)));
+    for (uint i = 0u; i < uSteps; i++) {
+        float dist = random_exponential(state, uExtinction);
+        photon.position += dist * photon.direction;
+
+        vec4 volumeSample = sampleVolumeColor(photon.position);
+
+        float PNull = 1.0 - volumeSample.a;
+        float PScattering;
+        if (photon.bounces >= uMaxBounces) {
+            PScattering = 0.0;
+        } else {
+            PScattering = volumeSample.a * max3(volumeSample.rgb);
+        }
+        float PAbsorption = 1.0 - PNull - PScattering;
+
+        float fortuneWheel = random_uniform(state);
+        if (any(greaterThan(photon.position, vec3(1))) || any(lessThan(photon.position, vec3(0)))) {
+            // out of bounds
+            vec4 envSample = sampleEnvironmentMap(photon.direction);
+            vec3 radiance = photon.transmittance * envSample.rgb;
+            photon.samples++;
+            photon.radiance += (radiance - photon.radiance) / float(photon.samples);
+            resetPhoton(state, photon);
+        } else if (fortuneWheel < PAbsorption) {
+            // absorption
+            vec3 radiance = vec3(0);
+            photon.samples++;
+            photon.radiance += (radiance - photon.radiance) / float(photon.samples);
+            resetPhoton(state, photon);
+        } else if (fortuneWheel < PAbsorption + PScattering) {
+            // scattering
+            photon.transmittance *= volumeSample.rgb;
+            photon.direction = sampleHenyeyGreenstein(state, uAnisotropy, photon.direction);
+            photon.bounces++;
+        } else {
+            // null collision
+        }
+    }
+
+    oPosition = vec4(photon.position, 0);
+    oDirection = vec4(photon.direction, float(photon.bounces));
+    oTransmittance = vec4(photon.transmittance, 0);
+    // oRadiance = vec4(photon.radiance, float(photon.samples));
+    oRadiance = vec4(1);
+
+    oPositionRay = vPosition;
 }
 
 // #part /glsl/shaders/renderers/BASIC/render/vertex
@@ -172,20 +322,20 @@ void main() {
 precision mediump float;
 precision mediump sampler2D;
 
-uniform sampler2D uPosition;
+uniform sampler2D uPositionRay;
 
 out vec2 vPosition;
 
 void main() {
     float y = float(100 * int(float(gl_VertexID) / 100.0)) / 100000.0;
-    float x = float(gl_VertexID% 100) / 100.0;
+    float x = float(gl_VertexID % 100) / 100.0;
     vec2 pos = vec2(x, y);
 
-    vec2 position = texture(uPosition, pos).xy;
+    vec2 position = texture(uPositionRay, pos).xy;
 
     vPosition = pos;
     gl_Position = vec4(position, 0, 1);
-    gl_PointSize = 2.0;
+    gl_PointSize = 1.0;
 }
 
 // #part /glsl/shaders/renderers/BASIC/render/fragment
--- src/js/renderers/BasicRenderer.js
+++ src/js/renderers/BasicRenderer.js
@@ -15,6 +15,51 @@ export class BasicRenderer extends AbstractRenderer {
     constructor(gl, volume, camera, environmentTexture, options = {}) {
         super(gl, volume, camera, environmentTexture, options);
 
+        this.registerProperties([
+            {
+                name: 'extinction',
+                label: 'Extinction',
+                type: 'spinner',
+                value: 1,
+                min: 0,
+            },
+            {
+                name: 'anisotropy',
+                label: 'Anisotropy',
+                type: 'slider',
+                value: 0,
+                min: -1,
+                max: 1,
+            },
+            {
+                name: 'bounces',
+                label: 'Max bounces',
+                type: 'spinner',
+                value: 8,
+                min: 0,
+            },
+            {
+                name: 'stepsMip',
+                label: 'StepsMip',
+                type: 'spinner',
+                value: 1,
+                min: 0,
+            },
+            {
+                name: 'stepsMCM',
+                label: 'StepsMCM',
+                type: 'spinner',
+                value: 8,
+                min: 0,
+            },
+            {
+                name: 'transferFunction',
+                label: 'Transfer function',
+                type: 'transfer-function',
+                value: new Uint8Array(256),
+            },
+        ]);
+
         this._programs = WebGL.buildPrograms(this._gl, SHADERS.renderers.BASIC, MIXINS);
     }
 
@@ -41,7 +86,7 @@ export class BasicRenderer extends AbstractRenderer {
         gl.bindTexture(gl.TEXTURE_2D, this._transferFunction);
         gl.uniform1i(uniforms.uTransferFunction, 1);
 
-        gl.uniform1f(uniforms.uStepSize, 1 / this.steps);
+        gl.uniform1f(uniforms.uStepSize, 1 / this.stepsMip);
         gl.uniform1f(uniforms.uOffset, Math.random());
 
         const centerMatrix = mat4.fromTranslation(mat4.create(), [-0.5, -0.5, -0.5]);
@@ -71,9 +116,61 @@ export class BasicRenderer extends AbstractRenderer {
         gl.generateMipmap(gl.TEXTURE_2D);
         gl.uniform1i(uniforms.uFrame, 0);
 
+        gl.activeTexture(gl.TEXTURE1);
+        gl.bindTexture(gl.TEXTURE_2D, this._accumulationBuffer.getAttachments().color[0]);
+        gl.uniform1i(uniforms.uPosition, 1);
+
+        gl.activeTexture(gl.TEXTURE2);
+        gl.bindTexture(gl.TEXTURE_2D, this._accumulationBuffer.getAttachments().color[1]);
+        gl.uniform1i(uniforms.uDirection, 2);
+
+        gl.activeTexture(gl.TEXTURE3);
+        gl.bindTexture(gl.TEXTURE_2D, this._accumulationBuffer.getAttachments().color[2]);
+        gl.uniform1i(uniforms.uTransmittance, 3);
+
+        gl.activeTexture(gl.TEXTURE4);
+        gl.bindTexture(gl.TEXTURE_2D, this._accumulationBuffer.getAttachments().color[3]);
+        gl.uniform1i(uniforms.uRadiance, 4);
+
+        gl.activeTexture(gl.TEXTURE5);
+        gl.bindTexture(gl.TEXTURE_3D, this._volume.getTexture());
+        gl.uniform1i(uniforms.uVolume, 5);
+
+        gl.activeTexture(gl.TEXTURE6);
+        gl.bindTexture(gl.TEXTURE_2D, this._environmentTexture);
+        gl.uniform1i(uniforms.uEnvironment, 6);
+
+        gl.activeTexture(gl.TEXTURE7);
+        gl.bindTexture(gl.TEXTURE_2D, this._transferFunction);
+        gl.uniform1i(uniforms.uTransferFunction, 7);
+
+        gl.uniform2f(uniforms.uInverseResolution, 1 / this._resolution, 1 / this._resolution);
+        gl.uniform1f(uniforms.uRandSeed, Math.random());
+        gl.uniform1f(uniforms.uBlur, 0);
+
+        gl.uniform1f(uniforms.uExtinction, this.extinction);
+        gl.uniform1f(uniforms.uAnisotropy, this.anisotropy);
+        gl.uniform1ui(uniforms.uMaxBounces, this.bounces);
+        gl.uniform1ui(uniforms.uSteps, this.stepsMCM);
+
+        const centerMatrix = mat4.fromTranslation(mat4.create(), [-0.5, -0.5, -0.5]);
+        const modelMatrix = this._volumeTransform.globalMatrix;
+        const viewMatrix = this._camera.transform.inverseGlobalMatrix;
+        const projectionMatrix = this._camera.getComponent(PerspectiveCamera).projectionMatrix;
+        const matrix = mat4.create();
+        mat4.multiply(matrix, centerMatrix, matrix);
+        mat4.multiply(matrix, modelMatrix, matrix);
+        mat4.multiply(matrix, viewMatrix, matrix);
+        mat4.multiply(matrix, projectionMatrix, matrix);
+        mat4.invert(matrix, matrix);
+        gl.uniformMatrix4fv(uniforms.uMvpInverseMatrix, false, matrix);
+
         gl.drawBuffers([
             gl.COLOR_ATTACHMENT0,
             gl.COLOR_ATTACHMENT1,
+            gl.COLOR_ATTACHMENT2,
+            gl.COLOR_ATTACHMENT3,
+            gl.COLOR_ATTACHMENT4,
         ]);
 
         gl.drawArrays(gl.POINTS, 0, 100000);
@@ -86,12 +183,12 @@ export class BasicRenderer extends AbstractRenderer {
         gl.useProgram(program);
 
         gl.activeTexture(gl.TEXTURE0);
-        gl.bindTexture(gl.TEXTURE_2D, this._accumulationBuffer.getAttachments().color[0]);
+        gl.bindTexture(gl.TEXTURE_2D, this._accumulationBuffer.getAttachments().color[3]);
         gl.uniform1i(uniforms.uColor, 0);
 
         gl.activeTexture(gl.TEXTURE1);
-        gl.bindTexture(gl.TEXTURE_2D, this._accumulationBuffer.getAttachments().color[1]);
-        gl.uniform1i(uniforms.uPosition, 1);
+        gl.bindTexture(gl.TEXTURE_2D, this._accumulationBuffer.getAttachments().color[4]);
+        gl.uniform1i(uniforms.uPositionRay, 1);
 
         gl.drawArrays(gl.POINTS, 0, 100000);
     }
@@ -106,6 +203,9 @@ export class BasicRenderer extends AbstractRenderer {
             gl.COLOR_ATTACHMENT0,
             gl.COLOR_ATTACHMENT1,
         ]);
+
+        // gl.clearColor(0, 0, 0, 1);
+        // gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
         gl.drawArrays(gl.TRIANGLES, 0, 3);
     }
@@ -125,7 +225,9 @@ export class BasicRenderer extends AbstractRenderer {
 
     _getAccumulationBufferSpec() {
         const gl = this._gl;
-        const colorBufferSpec = {
+
+        // NOTE: Multiple attachments require SAME DIMENSIONS!
+        const positionBufferSpec = {
             width: this._resolution,
             height: this._resolution,
             min: gl.NEAREST,
@@ -135,7 +237,37 @@ export class BasicRenderer extends AbstractRenderer {
             type: gl.FLOAT,
         };
 
-        const positionBufferSpec = {
+        const directionBufferSpec = {
+            width: this._resolution,
+            height: this._resolution,
+            min: gl.NEAREST,
+            mag: gl.NEAREST,
+            format: gl.RGBA,
+            iformat: gl.RGBA32F,
+            type: gl.FLOAT,
+        };
+
+        const transmittanceBufferSpec = {
+            width: this._resolution,
+            height: this._resolution,
+            min: gl.NEAREST,
+            mag: gl.NEAREST,
+            format: gl.RGBA,
+            iformat: gl.RGBA32F,
+            type: gl.FLOAT,
+        };
+
+        const radianceBufferSpec = {
+            width: this._resolution,
+            height: this._resolution,
+            min: gl.NEAREST,
+            mag: gl.NEAREST,
+            format: gl.RGBA,
+            iformat: gl.RGBA32F,
+            type: gl.FLOAT,
+        };
+
+        const rayPositionBufferSpec = {
             width: this._resolution,
             height: this._resolution,
             min: gl.NEAREST,
@@ -146,8 +278,11 @@ export class BasicRenderer extends AbstractRenderer {
         };
 
         return [
-            colorBufferSpec,
             positionBufferSpec,
+            directionBufferSpec,
+            transmittanceBufferSpec,
+            radianceBufferSpec,
+            rayPositionBufferSpec,
         ];
     }
 
